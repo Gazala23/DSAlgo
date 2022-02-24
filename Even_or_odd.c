4715db2ef9b69138ef7f93971913aaea6b34f491
Gazala Perween <92989948+Gazala23@users.noreply.github.com>
2022-02-24 11:04:29 +0530

    Add files via upload

---
+++ Even_or_odd.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main (void)
+{
+    int a;
+    printf("Enter a number: ");
+    scanf(" %d\n", a)
+    if (a%2==0)
+    {
+        printf("Number is even\n")
+    }
+    else  
+    }
+        printf("Numbre is odd\n");
+    }
+    return 0;
+}
