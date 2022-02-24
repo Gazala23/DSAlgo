7ac503dc3219c34c3cd79e07aeab1adf4aa13454
Gazala Perween <92989948+Gazala23@users.noreply.github.com>
2022-02-24 10:41:25 +0530

    Add files via upload

---
+++ product.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+    printf("enter two numbers to multiply: ");
+    int a, b;
+    scanf("%d%d", &a, &b);
+    printf("Product of two numbers are %d", a * b);
+    return 0;
+}
